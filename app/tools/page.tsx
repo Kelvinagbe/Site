6212b913fe5fd238e6761d3b3e809c279e5ab4d3
Kelvin Agbe <139677635+Kelvinagbe@users.noreply.github.com>
2025-05-26 04:16:03 +0100

    Update page.tsx

--- app/tools/page.tsx
+++ app/tools/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import AIA from './component/AIAAssistant';
+import AIA from './components/AIAAssistant';
 
 // Individual app components
 const Dashboard = () => (
