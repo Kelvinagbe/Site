3078854ba187baf6c702caab85f9a6f4cc4eb2f3
Kelvin Agbe <139677635+Kelvinagbe@users.noreply.github.com>
2025-06-12 11:31:01 +0100

    Create index.ts

---
+++ app/tools/components/image/index.ts
@@ -0,0 +1,7 @@
+// Export all image tools
+export { ImageGenerator } from './generate';
+
+// Future exports:
+// export { ImageEditor } from './editor';
+// export { ImageUpscaler } from './upscaler';
+// export { ImageConverter } from './converter';
