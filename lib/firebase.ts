4ffde2f3dbbbe6e545cd100d7f86aa9ce8d1316b
Kelvin Agbe <139677635+Kelvinagbe@users.noreply.github.com>
2025-06-12 02:16:39 +0100

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,6 +1,8 @@
 // lib/firebase.ts
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -13,4 +15,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const realtimeDb = getDatabase(app);
 export default app;
