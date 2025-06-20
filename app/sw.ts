bc0e2bc72f1436d1a1725e1d47a508c876c26adf
Kelvin Agbe <139677635+Kelvinagbe@users.noreply.github.com>
2025-06-20 02:04:41 +0100

    Create sw.ts

---
+++ app/sw.ts
@@ -0,0 +1,35 @@
+import { defaultCache } from '@serwist/next/worker';
+import type { PrecacheEntry, SerwistGlobalConfig } from 'serwist';
+import { Serwist } from 'serwist';
+
+// This declares the value of `self.SW_VERSION` to TypeScript
+declare global {
+  interface WorkerGlobalScope extends SerwistGlobalConfig {
+    SW_VERSION: string;
+  }
+}
+
+declare const self: ServiceWorkerGlobalScope;
+
+const serwist = new Serwist({
+  precacheEntries: self.__SW_MANIFEST,
+  skipWaiting: true,
+  clientsClaim: true,
+  navigationPreload: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/.*\/tools.*/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'tools-cache',
+        expiration: {
+          maxEntries: 10,
+          maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+        },
+      },
+    },
+    ...defaultCache,
+  ],
+});
+
+serwist.addEventListeners();
